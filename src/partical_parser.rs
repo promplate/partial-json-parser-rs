bcf60c6c1655583595806d93c1fd2fcd8d3a2e25
徐铭 <xumingxs@qq.com>
2024-04-07 18:21:21 +0800

    结构修改，实现parse_spec

--- src/partical_parser.rs
+++ src/partical_parser.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 
+use nom::branch::alt;
 use nom::bytes::complete::{escaped, tag, take, take_till, take_while, take_while1};
 use nom::character::complete::{char, one_of};
 use nom::combinator::{complete, cut, map, map_res};
@@ -7,13 +8,13 @@ use nom::error::{FromExternalError, ParseError, VerboseError};
 use nom::sequence::{delimited, preceded, separated_pair, terminated, tuple};
 use nom::{IResult, Offset};
 
-use crate::debug_println;
+use crate::{debug_println, utils};
 
-#[derive(Debug, PartialEq, Eq, Default)]
+#[derive(Debug, PartialEq, Eq, Default, Clone, Copy)]
 pub enum JsonType {
     #[default]
     Str,
-    Boolean,
+    Spec,
     Num,
     KeyVal,
     Array,
@@ -27,18 +28,34 @@ enum ErrType {
     Completion {
         json_type: JsonType,
         completion: String,
-        last_completion: String,
     },
 }
 
 type ParseRes<'a, O> = IResult<&'a str, O, ErrRes<'a, &'a str>>;
 
 trait ErrCast<'a> {
-    fn cast(self, cur_str: &'a str, last_completion: String, json_type: JsonType) -> Self;
+    fn cast(
+        self,
+        cur_str: &'a str,
+        cur_completion: &str,
+        json_type: JsonType,
+        delete: bool,
+    ) -> Self;
+    fn func_cast<F>(self, func: F) -> Self
+    where
+        F: Fn(&mut ErrRes<'a, &str>);
+    fn is_invalid(&self) -> bool;
+    fn is_incomplete(&self) -> bool;
 }
 
 impl<'a, O> ErrCast<'a> for ParseRes<'a, O> {
-    fn cast(mut self, cur_str: &'a str, last_completion: String, json_type: JsonType) -> Self {
+    fn cast(
+        mut self,
+        cur_str: &'a str,
+        cur_completion: &str,
+        json_type: JsonType,
+        delete: bool,
+    ) -> Self {
         if let Err(ref mut err) = self {
             match err {
                 nom::Err::Error(err) | nom::Err::Failure(err) => {
@@ -46,21 +63,76 @@ impl<'a, O> ErrCast<'a> for ParseRes<'a, O> {
                         // 如果是第一次发生错误，需要将err_str记录下来
                         let (rem, _) = err.base_err.errors.split_first().unwrap();
                         debug_println!("rem: {}", rem.0);
+                        let completion = if !delete {
+                            cur_completion.to_string()
+                        } else {
+                            String::new()
+                        };
                         err.err_type = if rem.0.is_empty() {
                             ErrType::Completion {
-                                completion: String::default(),
+                                completion,
                                 json_type,
                                 // 所有的completion都在返回上一级后提交（可能会丢弃）
-                                last_completion,
                             }
                         } else {
                             ErrType::Failure
                         };
                         err.err_str = Some(cur_str)
+                    } else {
+                        // 不是第一次发生错误，做push_str直接将错误向后传递即可
+                        if let ErrType::Completion {
+                            ref mut completion, ..
+                        } = err.err_type
+                        {
+                            if !delete {
+                                completion.push_str(cur_completion)
+                            }
+                        }
+                    }
+                }
+                _ => panic!("should not reach here: Incomplete Arm"),
+            }
+        }
+        self
+    }
+
+    fn is_invalid(&self) -> bool {
+        let mut invalid = false;
+        if let Err(ref err) = self {
+            match err {
+                nom::Err::Error(err) | nom::Err::Failure(err) => {
+                    if err.err_str.is_none() {
+                        let (rem, _) = err.base_err.errors.split_first().unwrap();
+                        invalid = !rem.0.is_empty();
+                    } else {
+                        invalid = err.err_type == ErrType::Failure;
+                    }
+                }
+                _ => panic!("should not reach here: Incomplete Arm"),
+            }
+        }
+        invalid
+    }
+
+    // 这里其实是有点问题的，对于非一个字符一个字符的匹配，它得到的结论是错误的
+    // 比如说对于tag来说
+    fn is_incomplete(&self) -> bool {
+        self.is_err() && !self.is_invalid()
+    }
+
+    fn func_cast<F>(mut self, func: F) -> Self
+    where
+        F: Fn(&mut ErrRes<'a, &str>),
+    {
+        if let Err(ref mut err) = self {
+            match err {
+                nom::Err::Error(err) | nom::Err::Failure(err) => {
+                    if err.err_str.is_none() {
+                        func(err);
                     }
                     // 不是第一次发生错误，不做处理直接将错误向后传递即可
                 }
-                _ => panic!("should not reach here"),
+                _ => panic!("should not reach here: Incomplete Arm"),
             }
         }
         self
@@ -117,12 +189,7 @@ fn parse_string(i: &str) -> ParseRes<&str> {
             char('\"'),
         ),
     )(i);
-    res.cast(i, String::from("\""), JsonType::Str)
-}
-
-fn tryd(i: &str) -> ParseRes<&str> {
-    let res = terminated(parse_string, char('\"'))(i);
-    res.cast(i, String::from("ds"), JsonType::Str)
+    res.cast(i, "\"", JsonType::Str, false)
 }
 
 fn parse_key_value(i: &str) -> ParseRes<(&str, &str)> {
@@ -132,17 +199,78 @@ fn parse_key_value(i: &str) -> ParseRes<(&str, &str)> {
         cut(preceded(sp, char(':'))),
         parse_string,
     )(i);
-    res.cast(i, "".to_string(), JsonType::KeyVal)
+    res.cast(i, "", JsonType::KeyVal, true)
 }
 
-fn parse_boolean(i: &str) -> ParseRes<&str> {
-    todo!()
+fn parse_spec(i: &str) -> ParseRes<&str> {
+    // 对于非一个一个字符的匹配，incomplete的语义是不一样的
+    // 对于alt使用，如果不是最终的alt，一定要改写它的error_type
+    let res: ParseRes<&str> = alt((
+        tag("false"),
+        tag("true"),
+        tag("NaN"),
+        tag("Null"),
+        tag("Infinity"),
+        tag("-Infinity"),
+    ))(i);
+    let spec_vec = [
+        ("true", 1),
+        ("false", 1),
+        ("NaN", 2),
+        ("Null", 1),
+        ("Infinity", 1),
+        ("-Infinity", 2),
+    ];
+    res.func_cast(|err_res| {
+        err_res.err_str = Some(i);
+        let completion = spec_vec
+            .iter()
+            .find_map(|(pattern, min_len)| {
+                if utils::is_prefix_with_min_length(pattern, i, *min_len) {
+                    utils::complement_after(pattern, i)
+                } else {
+                    None
+                }
+            })
+            .unwrap_or("");
+        err_res.err_type = if completion.is_empty() {
+            ErrType::Failure
+        } else {
+            // 这是最小的一级，所以不需要考虑append
+            ErrType::Completion {
+                completion: completion.to_string(),
+                json_type: JsonType::Spec,
+            }
+        }
+    })
+}
+
+#[allow(unused)]
+macro_rules! quick_test_failed {
+    ($input: expr, $func: ident, $($eq_left: expr => $field: ident), +) => {{
+        let output = $func($input);
+        match output {
+            Err(nom::Err::Error(err)) | Err(nom::Err::Failure(err)) => {
+                $(
+                    assert_eq!($eq_left, err.$field);
+                )+
+            }
+            _ => panic!("Output is ok or completion!"),
+        }
+    }};
+}
+
+#[allow(unused)]
+macro_rules! quick_test_ok {
+    ($input: expr, $func: ident, $eq_left: expr) => {{
+        let output = $func($input);
+        assert_eq!($eq_left, output);
+    }};
 }
 
 #[cfg(test)]
-mod tests {
+mod test_string {
     use super::*;
-    use crate::debug_println;
 
     #[test]
     fn test_string_complete_without_escaped() {
@@ -184,8 +312,7 @@ mod tests {
             Err(nom::Err::Error(err)) | Err(nom::Err::Failure(err)) => {
                 assert_eq!(
                     ErrType::Completion {
-                        completion: String::default(),
-                        last_completion: r#"""#.to_string(),
+                        completion: r#"""#.to_string(),
                         json_type: JsonType::Str
                     },
                     err.err_type
@@ -195,27 +322,17 @@ mod tests {
             _ => panic!("Output is ok or completion!"),
         }
     }
+}
 
-    macro_rules! quick_test_failed {
-        ($input: expr, $func: ident, $($eq_left: expr => $field: ident), +) => {{
-            let output = $func($input);
-            match output {
-                Err(nom::Err::Error(err)) | Err(nom::Err::Failure(err)) => {
-                    $(
-                        assert_eq!($eq_left, err.$field);
-                    )+
-                }
-                _ => panic!("Output is ok or completion!"),
-            }
-        }}; 
-    }
+#[cfg(test)]
+mod test_keyval {
+    use super::*;
 
     #[test]
     fn test_keyval_incomplete() {
-        quick_test_failed!(r#""laljfw""#, parse_key_value, 
+        quick_test_failed!(r#""laljfw""#, parse_key_value,
             ErrType::Completion {
-                completion: String::default(),
-                last_completion: r#""#.to_string(),
+                completion: r#""#.to_string(),
                 json_type: JsonType::KeyVal
             } => err_type,
             Some(r#""laljfw""#) => err_str
@@ -224,9 +341,103 @@ mod tests {
 
     #[test]
     fn test_keyval_invalid() {
-        quick_test_failed!(r#""laljfw" ("#, parse_key_value, 
+        quick_test_failed!(r#""laljfw" ("#, parse_key_value,
             ErrType::Failure => err_type,
             Some(r#""laljfw" ("#) => err_str
         )
+    }
+}
+
+#[cfg(test)]
+mod test_boolean {
+    use super::*;
+
+    #[test]
+    fn test_bool_ok() {
+        quick_test_ok!("true", parse_spec, Ok(("", "true")));
+        quick_test_ok!("false", parse_spec, Ok(("", "false")));
+        quick_test_ok!("NaN", parse_spec, Ok(("", "NaN")));
+        quick_test_ok!("Null", parse_spec, Ok(("", "Null")));
+        quick_test_ok!("Infinity", parse_spec, Ok(("", "Infinity")));
+        quick_test_ok!("-Infinity", parse_spec, Ok(("", "-Infinity")));
+    }
+
+    #[test]
+    fn test_bool_incomplete() {
+        quick_test_failed!("tr", parse_spec,
+            ErrType::Completion {
+                completion: "ue".to_string(),
+                json_type: JsonType::Spec,
+            } => err_type,
+            Some("tr") => err_str
+        );
+
+        quick_test_failed!("fal", parse_spec,
+            ErrType::Completion {
+                completion: "se".to_string(),
+                json_type: JsonType::Spec,
+            } => err_type,
+            Some("fal") => err_str
+        );
+
+        quick_test_failed!("Na", parse_spec,
+            ErrType::Completion {
+                completion: "N".to_string(),
+                json_type: JsonType::Spec,
+            } => err_type,
+            Some("Na") => err_str
+        );
+
+        quick_test_failed!("N", parse_spec,
+            ErrType::Completion {
+                completion: "ull".to_string(),
+                json_type: JsonType::Spec,
+            } => err_type,
+            Some("N") => err_str
+        );
+
+        quick_test_failed!("Inf", parse_spec,
+            ErrType::Completion {
+                completion: "inity".to_string(),
+                json_type: JsonType::Spec,
+            } => err_type,
+            Some("Inf") => err_str
+        );
+
+        quick_test_failed!("-I", parse_spec,
+            ErrType::Completion {
+                completion: "nfinity".to_string(),
+                json_type: JsonType::Spec,
+            } => err_type,
+            Some("-I") => err_str
+        );
+    }
+
+    #[test]
+    fn test_bool_invalid() {
+        quick_test_failed!("-", parse_spec,
+            ErrType::Failure => err_type,
+            Some("-") => err_str
+        );
+
+        quick_test_failed!("tu", parse_spec,
+            ErrType::Failure => err_type,
+            Some("tu") => err_str
+        );
+
+        quick_test_failed!("", parse_spec,
+            ErrType::Failure => err_type,
+            Some("") => err_str
+        );
+
+        quick_test_failed!("fl", parse_spec,
+            ErrType::Failure => err_type,
+            Some("fl") => err_str
+        );
+
+        quick_test_failed!("Nau", parse_spec,
+            ErrType::Failure => err_type,
+            Some("Nau") => err_str
+        );
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -15,3 +15,11 @@ macro_rules! debug_print {
         }
     };
 }
+
+pub fn complement_after<'a>(full: &'a str, part: &'a str) -> Option<&'a str> {
+    full.find(part).map(|index| &full[index + part.len()..])
+}
+
+pub fn is_prefix_with_min_length(str1: &str, str2: &str, min_length: usize) -> bool {
+    str1.starts_with(str2) && str2.len() >= min_length
+}
