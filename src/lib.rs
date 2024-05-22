fdaac3a7cbd178ca33e91f49b1d32fdf0cadff83
Ming Xu <xumingxs@qq.com>
2024-05-22 21:12:39 +0800

    bump pyo3 version

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 }
 
 #[pymodule]
-fn partial_json_parser_rs(_py: Python, m: &PyModule) -> PyResult<()> {
+fn partial_json_parser_rs(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
 }
