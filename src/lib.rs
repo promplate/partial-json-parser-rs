0c46f9fb09ba292a75875092059d37579e73edb6
徐铭 <xumingxs@qq.com>
2024-06-02 10:22:30 +0800

    删除pyo3支持，这种功能应该在另外的库做

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,3 @@ mod parser;
 #[cfg(test)]
 mod test_utils;
 mod utils;
-
-use pyo3::prelude::*;
-
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
-#[pymodule]
-fn partial_json_parser_rs(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-    Ok(())
-}
